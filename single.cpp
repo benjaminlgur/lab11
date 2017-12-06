214016830a457782d5ea2281c20706b2f2391f60
benjaminlgur <benjaminlgur@yahoo.com>
2017-12-05 20:15:17 -0500

    commented just need test cases

--- single.cpp
+++ single.cpp
@@ -4,10 +4,20 @@
 using namespace std;
 
 bool isEven(string s);
-bool endCheck(string s);
+//Precondition: s is a string with bracket symbols other ascii charecters may be included.
+//Postconditon: Will return true if there is an even amount of bracket symbols in s.
+bool wellFormed(string s);
+////Precondition: s is a string with bracket symbols other ascii charecters may be included.
+//Postconditon: Will return true if s is considered wellformed.
 int nestLevel(string s);
+//Precondition: s is a string with bracket symbols other ascii charecters may be included but must be wellformed.
+//Postcondition: will return an int of how many levels of nesting within the string s.
 int lengthCheck(string s);
-char bracket(char thing);
+//Precondition:  s is a string with bracket symbols other ascii charecters may be included.
+//Postcondition: Will return an int with the number of brackets within the string s.
+char bracket(char letter);
+//Precondition: letter is a char obtained by a portion of the string s such as s[i]
+//Postcondition: will return 'f' if letter is a front bracket b if back bracket c if another kind of charecter.
 
 int main(){
     string s;
@@ -18,7 +28,7 @@ int main(){
         cout << "Not well-formed\n"; //must be even in order to have all pairs therefore wellformed
         return 0;
     }
-    if(endCheck(s) == true){;
+    if(wellFormed(s) == true){;
         cout << "Well-formed\n" << "The string has " << nestLevel(s) << " levels of nesting.\n";
     }
     else {
@@ -26,7 +36,7 @@ int main(){
     }
 }
 
-bool endCheck(string s){ //subtracted length by one becuse of 0 indexing.
+bool wellFormed(string s){ 
     static int count = 0;
     static int left = 0;
     static int right = 0;
@@ -34,29 +44,29 @@ bool endCheck(string s){ //subtracted length by one becuse of 0 indexing.
         return true;
     }
     if(s[count + left] == '{' && s[(s.length() - 1) - count - right] == '}'){ 
-        count++;
-        endCheck(s);
+        count++; //subtracted s.length() by one due to 0 indexing.
+        wellFormed(s);
     }
     else if(s[count + left] == '[' && s[(s.length() - 1) - count - right] == ']'){
         count++;
-        endCheck(s);
+        wellFormed(s);
     }
     else if(s[count + left] == '<' && s[(s.length() - 1) - count - right] == '>'){
         count++;
-        endCheck(s);
+        wellFormed(s);
     }
     else if(s[count + left] == '(' && s[(s.length() - 1) - count - right] == ')'){
         count++;
-        endCheck(s);
+        wellFormed(s);
     }
     else {
         if(bracket(s[count + left]) == 'c'){
             left++;
-            endCheck(s);
+            wellFormed(s);
         }
         else if(bracket(s[s.length() - 1 - count - right]) == 'c'){
             right++;
-            endCheck(s);
+            wellFormed(s);
         }
         else if(bracket(s[count + left]) == 'b' || bracket(s[s.length() - 1 - count - right]) == 'f'){
             return false;
@@ -87,11 +97,11 @@ int lengthCheck(string s){
     return val;
 }
 
-char bracket(char thing){ //f is front bracket b is back bracket c is a difrent kind of char.
-    if(thing == '{' || thing == '[' || thing == '(' || thing == '<'){
+char bracket(char letter){ //f is front bracket b is back bracket c is a another kind of char.
+    if(letter == '{' || letter == '[' || letter == '(' || letter == '<'){
         return 'f';
     }
-    else if (thing == '}' || thing == ']' || thing == ')' || thing == '>'){
+    else if (letter == '}' || letter == ']' || letter == ')' || letter == '>'){
         return 'b';
     }
     else {
